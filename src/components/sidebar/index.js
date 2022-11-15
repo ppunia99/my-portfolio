b3010039e431639548b2f15c6640f9813ea5f5e7
Prith <prith1030@gmail.com>
2022-11-15 13:36:40 -0800

    removed imports

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,8 +1,7 @@
 import './index.scss'
 import { Link } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faUser,
